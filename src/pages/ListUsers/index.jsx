c87c955d0e827285d4cf698bebcfd2ca2e7fa947
Flavio <flavio.tomas.villa@gmail.com>
2026-02-10 14:49:53 -0300

    Utilizando useEffect para toda vez que a pagina de usuarios carregar, pegar os dados do banco de dado, e utilizando useState para armazenar esses e dados e imprimir na tela

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -4,20 +4,42 @@ import UsersImg from '../../assets/users.png'
 import { useNavigate } from 'react-router-dom'
 import DefaultButton from '../../components/Button'
 import api from '../../services/api'
+import { useEffect, useState } from 'react'
 
 
-function ListUsers(){
+
+function ListUsers() {
     const navigate = useNavigate()
+    const [ users, setUsers ] = useState([])
 
-    
+    useEffect(() => {
+
+        async function getUsers() {
+            const { data } = await api.get('/users')
+            setUsers(data)
+        }
+        getUsers()
+    }, [])
 
 
-    return(
 
-        
+    return (
+
+
         <DefaultContainer>
             <DefaultImgBackground><img src={UsersImg} alt="image-users" /></DefaultImgBackground>
             <h1>Listagem de Usuários</h1>
+
+
+            {users.map( user => (
+                <div>
+                    <p>{user.name}</p>
+                    <p>{user.email}</p>
+                    <p>{user.age}</p>
+                </div>
+            ))}
+
+
             <DefaultButton type="button" onClick={() => navigate('/')}>Voltar</DefaultButton>
         </DefaultContainer>
     )
